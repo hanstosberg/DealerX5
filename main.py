5c277c84187c53130e6f9aaf00cb024ddfced92c
hanst <hanstosberg@gmail.com>
2024-10-21 21:29:34 +0300

    Some adds, no func

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import random
 
-
-#start_title():
+# start_title():
 print("DEALER'S CHOICE")
 print("CREATIVE COMPUTING")
 print("MORRISTOWN, NEW JERSEY")
@@ -9,8 +8,7 @@ print()
 print("THIS PROGRAM WILL SIMULATE THE T.V. SHOW, DEALER'S CHOICE.")
 print("YOU HAVE $100 TO START WITH. ENJOY THE GAME.")
 
-
-#first_game():
+# first_game():
 O = 100
 print("FOR THE FIRST GAME, WE WILL PLAY ON THE WHEEL OF CHANCE.")
 print("THE OBJECT IS SIMPLE. GUESS WHAT SUITE WILL APPEAR ON THE")
@@ -89,7 +87,7 @@ while True:
 
     T = T + 1
     Z = int(12 * random.random() + 1)
-    print("CARD NUMBER " , T , " IS A " , Z)
+    print("CARD NUMBER ", T, " IS A ", Z)
     B += (2 * A)
     if (Z < 3 or Z > 10):
         O = O - A
@@ -112,8 +110,7 @@ while True:
 for i in range(5):
     print()
 
-
-#third game
+# third game
 print("THIS IS THE GAME OF BLACKJACK <DEALER'S CHOICE STYLE>")
 print("THE OBJECT IS TO BEAT THE DEALER WITH OVER 17 OR 21 OR")
 print("UNDER. YOU MAY BET UP TO $50. YOU MAY STOP WHEN YOU WISH.")
@@ -129,10 +126,10 @@ C = 0
 while True:
     Q = int(12 * random.random() + 1)
     Z5 = Q + Z5
-    print("THE CARD IS A ",Q)
-    C=C+1
+    print("THE CARD IS A ", Q)
+    C = C + 1
     print("SO FAR:", Z5)
-    if C==6:
+    if C == 6:
         if Z5 < 17:
             print("THE HOUSE DELT OUT LESS THAN 17. NOW YOU MUST TRY TO")
             print("BEAT ME")
@@ -148,7 +145,7 @@ while True:
                 print("I GOT BLACKJACK")
                 print()
             if Z5 < 21:
-                print("I STOP. THE TOTAL FOR ME IS ",Z5)
+                print("I STOP. THE TOTAL FOR ME IS ", Z5)
                 print()
                 print("NOW YOU GO")
                 print()
@@ -165,7 +162,7 @@ while True:
             print("I GOT BLACKJACK")
             print()
         if Z5 < 21:
-            print("I STOP. THE TOTAL FOR ME IS ",Z5)
+            print("I STOP. THE TOTAL FOR ME IS ", Z5)
             print()
             print("NOW YOU GO")
             print()
@@ -176,7 +173,7 @@ while True:
         if (A > O):
             print("***YOU BET OVER WHAT YOU HAVE***")
             pass
-        if A>50 or A <=0:
+        if A > 50 or A <= 0:
             print("***YOU BET OVER THE HOUSE LIMIT***")
             pass
         break
@@ -186,11 +183,11 @@ while True:
 
     flag = False
     while True:
-        Q1= int(12 * random.random() + 1)
-        print("YOUR CARD IS A ",Q1)
+        Q1 = int(12 * random.random() + 1)
+        print("YOUR CARD IS A ", Q1)
         C3 = C3 + 1
         Z1 = Q1 + Z1
-        print("SO FAR THE TOTAL FOR YOU IS ",Z1)
+        print("SO FAR THE TOTAL FOR YOU IS ", Z1)
         if (C3 == 6):
             if Z1 < 17:
                 O += A
@@ -199,11 +196,12 @@ while True:
                 break
             else:
                 print("WE ARE THE SAME SO WE WILL PLAY AGAIN")
-                pass
+                continue
         else:
             if Z1 > 21:
                 O = O - (1 * A)
                 print("THE DEALER BEAT YOU. YOU LOSE")
+                flag = True
                 break
             else:
                 print("STOP OR GO")
@@ -214,34 +212,37 @@ while True:
                         Z5 = 0
                         C = 0
                         C3 = 0
-                        Z1 = 0                   
+                        Z1 = 0
+                        flag = False
                         break
                     if Z1 < Z5:
-                        O = O - (1*A)
+                        O = O - (1 * A)
                         print("THE DEALER BEAT YOU. YOU LOSE")
                         flag = True
                         break
                     if Z5 < Z1:
-                        O=O+(1*A)
+                        O = O + (1 * A)
                         print("THE DEALER LOST. YOU WIN")
                         flag = True
-                        break       
+                        break
                     if Z1 < 17:
-                        O=O+(1*A)
+                        O = O + (1 * A)
                         print("THE HOUSE DELT OUT LESS THAN 17 IN")
                         print("6 TRIES. YOU GET THE MONEY YOU BET")
                         flag = True
                         break
-                        
+
                 if AA == "GO":
                     continue
     if flag == True:
         break
+    if flag == False:
+        continue
 
-
-print("AT THE END OF PART 3, YOU HAVE $",O)
-if O <=0:
-    print("AT THE END OF THE GAME YOU HAVE A GRAND TOTAL OF $",O)
+print("AT THE END OF PART 3, YOU HAVE $", O)
+if O <= 0:
+    print("AT THE END OF THE GAME YOU HAVE A GRAND TOTAL OF $", O)
+    exit
 for i in range(5):
     print()
 
@@ -259,39 +260,42 @@ print("AT THIS POINT IN THE GAME YOU HAVE $", O)
 while True:
     print("WHAT CATEGORY DO YOU WANT")
     A = int(input())
-    if A > 4:
+    if A > 4 or A < 0:
         print("***YOU BET ON A WRONG CATEGORY***")
         continue
     if A <= 4:
-        break  
+        break
 
 while True:
     print("WHAT IS THE BET")
     B = int(input())
-    if B <=0:
+    if B <= O:
         print("THE CARDS ARE NOW BEING ADDED UP")
         print("GOOD LUCK!")
         break
-    if B > 0:
+    if B > O:
         print("***YOU BET OVER WHAT YOU HAVE***")
         continue
 C1 = 0
+Z = 0
 while True:
-    Q= int(12 * random.random() + 1)
-    C1=C1+1
-    print("CARD NUMBER ",C1," IS A ",Q)
-    Z=Z+Q
-    print("SO FAR: ",Z)
-    if C1==5:
-        if (A == 1 and Z < 31) and (A == 2 and 31 <= Z < 40) and  (A == 3 and 40 <= Z < 50) and (A == 4 and 50 <= Z):
-            O=O+(1*B)
-            print("YOU WIN")    
+    Q = int(12 * random.random() + 1)
+    C1 = C1 + 1
+    print("CARD NUMBER ", C1, " IS A ", Q)
+    Z = Z + Q
+    print("SO FAR: ", Z)
+    if C1 == 5:
+        if (A == 1 and Z < 31) and (A == 2 and 31 <= Z < 40) and (A == 3 and 40 <= Z < 50) and (A == 4 and 50 <= Z):
+            O = O + (1 * B)
+            print("YOU WIN")
+            break
+        else:
+            O = O - (1 * B)
+            print("YOU LOSE")
             break
     else:
-        O=O-(1*B)
-        print("YOU LOSE")
-        break
-print("AT THE END OF THE GAME YOU HAVE A GRAND TOTAL OF $",O) 
+        continue
+print("AT THE END OF THE GAME YOU HAVE A GRAND TOTAL OF $", O)
 if O < 300:
     print("THIS IS THE END OF THE GAME. I HOPE YOU ENJOYED IT.")
     exit
@@ -308,46 +312,48 @@ print("WITHOUT GETTING A SPADE IN THE ROLLS, YOU WILL GET")
 print("A GRAND PRIZE OF $10,000.00. YOU MAY STOP AT ANY POINT")
 print("DURING THE GAME. YOU WILL KEEP WHAT YOU MADE. GOOD LUCK!")
 print()
-A = [] * 6
-B = [] * 5 
+A = [0] * 6
+B = [0] * 5
 A9 = "SPADES"
 B7 = 0
-print("THE DICE ARE ROLLING")
-print("GOOD LUCK.")
-print("THE DICE ARE")
-X=int(5 * random.random() + 1)
-A[1]=50
-A[2]=100
-A[3]=150
-A[4]=200
-A[5]=0
-Y=int(4 * random.random() + 1)
-B[1]=50
-B[2]=100
-B[3]=150
-B[4]=200
-if A(X)==0:
-    print("****",A9,B(Y),"****")
-    print("TOTAL ",B(Y))
-print("****",A(X),B(Y),"****")
-print("TOTAL ",A(X)+B(Y))
-B7=B7+(A(X)+B(Y))
-print("YOU NOW HAVE ",B7)
-if B7 >= 1000:
-    for i in range(3):
-        print(" ")
-    B7=O+10000
-    print("               ","****CONGRATULATIONS****")
-    print("YOU WON THE GRAND PRIZE. AT THE END OF THE GAME, YOU HAVE") 
-    for i in range(3):
-        print()
-    print(("                  ","*******",B7,"******"))
+while True:
+    print("THE DICE ARE ROLLING")
+    print("GOOD LUCK.")
+    print("THE DICE ARE")
+    X = int(4 * random.random() + 1)
+    A[0] = 50
+    A[1] = 100
+    A[2] = 150
+    A[3] = 200
+    A[4] = 0
+    Y = int(3 * random.random() + 1)
+    B[0] = 50
+    B[1] = 100
+    B[2] = 150
+    B[4] = 200
+    if A[X] == 0:
+        print("****", A9, B[Y], "****")
+        print("TOTAL ", B[Y])
+    print("****", A[X], B[Y], "****")
+    print("TOTAL ", A[X] + B[Y])
+    B7 = B7 + A[X] + B[Y]
+    print("YOU NOW HAVE ", B7)
+    if B7 >= 1000:
+        for i in range(3):
+            print(" ")
+        B7 = O + 10000
+        print("               ", "****CONGRATULATIONS****")
+        print("YOU WON THE GRAND PRIZE. AT THE END OF THE GAME, YOU HAVE")
+        for i in range(3):
+            print()
+        print(("                  ", "*******", B7, "******"))
+        print("THIS IS THE END OF THE GAME. I HOPE YOU ENJOYED IT.")
+    print("STOP OR GO")
+    BB = str(input())
+    if BB == "GO":
+        continue
+    print("SMART MOVE. YOU GET THE MONEY FROM THE BEGINNING OF")
+    print("THE GAME PLUS THE BONUS ROUND. AT THE END OF THE GAME")
+    print("YOU HAVE THE GRAND TOTAL OF $", B7 + O)
     print("THIS IS THE END OF THE GAME. I HOPE YOU ENJOYED IT.")
-print("STOP OR GO")
-B = str(input())
-if B=="GO":
-    pass
-print("SMART MOVE. YOU GET THE MONEY FROM THE BEGINNING OF")
-print("THE GAME PLUS THE BONUS ROUND. AT THE END OF THE GAME")
-print("YOU HAVE THE GRAND TOTAL OF $",B7+O)
-print("THIS IS THE END OF THE GAME. I HOPE YOU ENJOYED IT.")
+    exit
